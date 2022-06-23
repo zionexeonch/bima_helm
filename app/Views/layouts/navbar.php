b984d2dd31d8a9a0204381ab31f1da31613bf5bf
Dzaky Fadli <zaaaafl654@gmail.com>
2022-06-23 14:42:55 +0700

    navbar

---
+++ app/Views/layouts/navbar.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-expand-md bg-light sticky-top">
+    <div class="container">
+        <div class="navbar-brand">
+            BIMA HELM
+        </div>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#nav-coll"
+        aria-controls="nav-coll" aria-expanded="false">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="nav-coll">
+            <div class="navbar-nav ms-auto">
+                <a href="#" class="nav-link">
+                    <i class="fa-solid fa-heart"></i>
+                </a>
+                <a href="#" class="nav-link">
+                    <i class="fa-solid fa-cart-shopping"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+</nav>
---
+++ app/Views/pages/index.php
@@ -0,0 +1,3 @@
+<?= $this->extend('layouts/template')?>
+<?= $this->section('content')?>
+<?= $this->endSection()?>
